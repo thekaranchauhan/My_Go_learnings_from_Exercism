ba047592148cb5d31f5b227a18ba54b9a5cc0cca
thekaranchauhan <chauhankaran2399@gmail.com>
2023-12-28 12:55:57 -0500

    EX3_gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -2,7 +2,7 @@ package gigasecond
 
 import "time"
 
-// AddGigasecond returns the time t+gigasecond.
+// AddGigasecond returns the time t + gigasecond.
 func AddGigasecond(t time.Time) time.Time {
 	return t.Add(time.Second * 1e9)
 }
