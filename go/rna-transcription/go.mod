35df00512f454672f7ee141275e2838f42df84b2
thekaranchauhan <chauhankaran2399@gmail.com>
2023-12-30 18:32:29 -0500

    EX13_rna-transcription

---
+++ go/rna-transcription/go.mod
@@ -0,0 +1,3 @@
+module strand
+
+go 1.21.5
---
+++ go/rna-transcription/rna_transcription.go
@@ -0,0 +1,19 @@
+package strand
+
+// ToRNA converts to RNA, duh
+func ToRNA(dna string) string {
+	retVal := ""
+	for _, ch := range dna {
+		switch ch {
+		case 'G':
+			retVal += "C"
+		case 'C':
+			retVal += "G"
+		case 'T':
+			retVal += "A"
+		case 'A':
+			retVal += "U"
+		}
+	}
+	return retVal
+}
